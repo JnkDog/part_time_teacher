a07cd90b1816ac82d0838558fc74059d408e0e95
yuyangovo <zhouccwinnie@163.com>
2021-05-17 16:42:29 +0100

    pttmodel

--- src/team/rapj/ptt/model/PTTModel.java
+++ src/team/rapj/ptt/model/PTTModel.java
@@ -1,10 +1,12 @@
 package src.team.rapj.ptt.model;
-import java.util.Date;
+
+import team.rapj.ptt.console.APPConsole;
+import team.rapj.ptt.dao.DataImpl;
+import java.util.Scanner;
 
 public class PTTModel {
     private String name;
-    private Date dateInfo; // 调用博哥时间结构
-    private String teacherID;
+    private int teacherID;
     
     public String getID() {
         return teacherID;
@@ -14,4 +16,18 @@ public class PTTModel {
         return name;
     }
 
+    public static void input() throws Exception {
+
+        Scanner scanner1 = new Scanner(System.in);
+        String t = scanner1.next();
+        String[] input = t.split(",");
+        this.name = input [0];
+        this.teacherID = Integer.parseInt (input [1]);
+    }
+
+    public static void output(){
+
+        System.out.print(input [0] + " " + input [1] );
+
+    }
 }
